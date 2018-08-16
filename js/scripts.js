32bd306230a6f6ec7876381c9b31464c8c52824e
Brian Nelson and Connor McCarthy <brian@>
2018-08-16 09:05:53 -0700

    pass third spec. Return first roman numeral of the decimal input

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 // business logic
 var decimalToRoman = function (inputDecimalNumber){
   inputDecimalNumber = parseInt(inputDecimalNumber);
-  return inputDecimalNumber;
+
+  var decimalValue = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+  var romanNumeral = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+  var outputVar;
+  for (i=0; i < romanNumeral.length; i++){
+    // alert(inputDecimalNumber + " " + i + ' ' + decimalValue[i]);
+    if (inputDecimalNumber >= decimalValue[romanNumeral.length - i]){
+      outputVar = romanNumeral[romanNumeral.length - i];
+      i = romanNumeral.length;
+      return outputVar;
+
+    }
+  }
 }
 // user logic
 $(document).ready(function() {
