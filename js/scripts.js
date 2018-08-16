d970d2c01e89d99c9a88b92fdc38bb95c62853ad
Brian Nelson and Connor McCarthy <connor@>
2018-08-16 08:31:57 -0700

    pass 1st spec. Input number and returns the number

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,13 @@
-var toBitNumber = function(txt, power) {
-  txt = parseInt(txt);
-  pwr = parseInt(power);
-  var bitArray = [];
-  for(;txt >= 1;) {
-    bitArray.push(txt % pwr);
-    txt = Math.floor(txt / pwr);
-  }
-  return bitArray.reverse().join('');
-};
-
-var toDecimal = function(txt) {
-  // txt = parseInt(txt);
-  var txt = txt.split('');
-  var hexAarray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G'];
-  var DecAarray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'];
-  var decimalArray = [];
-  for(i=0;i<3;i++){
-
-  }
-
+// business logic
+var decimalToRoman = function(inputDecimalNumber){
+  return inputDecimalNumber;
 }
-
-
-
+// user logic
 $(document).ready(function() {
   $("form#numberTranslator").submit(function(event) {
     event.preventDefault();
-
-    var userInputnum = $("input#inputNumber").val();
-    var userInputpwr = $("input#inputPower").val();
-    var bitOutput = toBitNumber(userInputnum, userInputpwr);
-    var decimalOutput = toDecimal(userInputnum);
-
-
-
-    $("#result").text("From decimal to base power " + userInputpwr + " is " + bitOutput);
-    $("#result2").text(decimalOutput);
+    var userInputToRoman = $("#inputNumber").val();
+    userInputToDecimal = decimalToRoman(userInputToRoman);
+    $("#result").text(userInputToDecimal);
   });
 });
