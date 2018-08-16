ca154a68f8044932167d48384bc176a7a3a07af9
Brian Nelson and Connor McCarthy <brian@>
2018-08-16 09:39:03 -0700

    pass fifth spec. Return roman numerals when decimal base numbers are input

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,28 @@ var decimalToRoman = function (inputDecimalNumber){
 
   var decimalValue = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
   var romanNumeral = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
-  var outputVar;
+  var outputVar = '';
   var remainder;
-  for (i=0; i < romanNumeral.length; i++){
-    // alert(inputDecimalNumber + " " + i + ' ' + decimalValue[i]);
-    if (inputDecimalNumber >= decimalValue[romanNumeral.length - i]){
-      outputVar = romanNumeral[romanNumeral.length - i];
-      remainder = inputDecimalNumber - decimalValue[romanNumeral.length - i];
-      i = romanNumeral.length;
-      return outputVar + " " + remainder;
+  for(;remainder !== 0;) {
+    for (i=0; i < romanNumeral.length + 1; i++){
+      // alert(inputDecimalNumber + " " + i + ' ' + decimalValue[i]);
+      // alert(inputDecimalNumber + " " + decimalValue[romanNumeral.length - i]);
+      if (inputDecimalNumber >= decimalValue[romanNumeral.length - i]){
 
+        outputVar = outputVar + romanNumeral[romanNumeral.length - i];
+        remainder = inputDecimalNumber - decimalValue[romanNumeral.length - i];
+
+        i = romanNumeral.length;
+      }
     }
+
+    inputDecimalNumber = remainder;
+
   }
+  return outputVar;
 }
+
+
 // user logic
 $(document).ready(function() {
   $("form#numberTranslator").submit(function(event) {
